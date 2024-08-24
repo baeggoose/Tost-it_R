06240ae1f3d529578318472376869ca651f74dcb
baeggoose <bsj154@gmail.com>
2024-08-24 23:30:40 +0900

    feat: db로 수정할일 불러오기, 수정 요청

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const { MongoClient } = require("mongodb");
+const { ObjectId } = require("mongodb");
 require("dotenv").config();
 
 let db;
@@ -29,7 +30,7 @@ app.get("/add", async (req, res) => {
     await db.collection("todo").insertOne({ title: "mongoDB 셋팅" });
     res.send("데이터가 성공적으로 추가되었습니다.");
   } catch (err) {
-    res.status(500).send("데이터 추가 중 오류 발생");
+    res.status(500).send("할일 추가 중 오류 발생");
   }
 });
 
@@ -38,6 +39,30 @@ app.get("/todo", async (req, res) => {
     let result = await db.collection("todo").find().toArray();
     res.json(result);
   } catch (err) {
-    res.status(500).send("데이터 가져오기 중 오류 발생");
+    res.status(500).send("할일 불러오기 중 오류 발생");
+  }
+});
+
+app.get("/edit/:id", async (req, res) => {
+  try {
+    let result = await db
+      .collection("todo")
+      .findOne({ _id: new ObjectId(req.params.id) });
+    res.json(result);
+  } catch (err) {
+    res.status(500).send("수정할 할일 불러오기 중 오류 발생");
+  }
+});
+app.get("/edit", async (req, res) => {
+  try {
+    let result = await db
+      .collection("todo")
+      .updateOne(
+        { _id: new ObjectId("66c9de1f3f1cbd90e280b36a") },
+        { $set: { title: "할일 수정", content: "수정되는지 테스트" } }
+      );
+    res.redirect("/todo");
+  } catch (err) {
+    res.status(500).send("할일 수정 중 오류 발생");
   }
 });
