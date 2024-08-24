62a1a6ccf56251c09c0b63c1f08a25af061c0515
baeggoose <bsj154@gmail.com>
2024-08-24 22:23:05 +0900

    chore: MongoDB와 서버 연결, 테스트 데이터 저장

--- server/server.js
+++ server/server.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const app = express();
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
-app.listen(8080, () => {
-  console.log("http://localhost:8080 에서 서버 실행중");
-});
+let db;
+const url = process.env.DB_URL;
+
+new MongoClient(url)
+  .connect()
+  .then((client) => {
+    console.log("DB연결성공");
+    db = client.db("Tost-it");
+
+    app.listen(8080, () => {
+      console.log("http://localhost:8080 에서 서버 실행중");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("express!!");
+});
+
+app.get("/todo", () => {
+  db.collection("todo").insertOne({ title: "mongoDB 셋팅" });
 });
