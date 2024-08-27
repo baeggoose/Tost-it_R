51de7eaa73ae9ab92b209eb69986e6b98d5fbc4c
baeggoose <bsj154@gmail.com>
2024-08-27 20:04:39 +0900

    feat: Todo 리스트 불러오기

--- client/src/pages/Todo/Todo.tsx
+++ client/src/pages/Todo/Todo.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+interface TodoItem {
+  _id: string;
+  title: string;
+  content?: string;
+}
 
 const Todo: React.FC = () => {
-  return <div>Todo</div>;
+  const [todo, setTodo] = useState<TodoItem[]>([]);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/todo");
+      const data = await response.json();
+      console.log(data);
+      setTodo(data);
+    } catch (error) {
+      console.error("데이터 불러오기 중 오류 발생", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+  return (
+    <>
+      <ul>
+        오늘 할일
+        {todo.map((todo) => (
+          <li key={todo._id}>
+            <h3>{todo.title}</h3>
+            <p>{todo.content}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default Todo;
