1f3cf6d0d53ff8c0aabfe40be1bc0d4f36a2d7b6
baeggoose <bsj154@gmail.com>
2024-09-09 18:13:18 +0900

    feat: 할일 추가나 불러올때 아침,점심,저녁 순으로 자동 정렬

--- client/src/components/Todo/TodoList.tsx
+++ client/src/components/Todo/TodoList.tsx
@@ -12,9 +12,22 @@ const TodoList: React.FC<TodoListProps> = ({
   onSaveEditTodo,
   onDeleteTodo,
 }) => {
+  const categoryOrder = {
+    morning: 1,
+    lunch: 2,
+    dinner: 3,
+  };
+
+  const sortedTodos = [...todos].sort((a, b) => {
+    return (
+      categoryOrder[a.category as keyof typeof categoryOrder] -
+      categoryOrder[b.category as keyof typeof categoryOrder]
+    );
+  });
+
   return (
     <ul className="h-fit max-h-450 pt-5 pb-5 pr-10 pl-10 grid grid-cols-2 gap-4 overflow-y-scroll">
-      {todos.map((todo) => (
+      {sortedTodos.map((todo) => (
         <TodoItem
           key={todo._id}
           todo={todo}
