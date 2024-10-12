623985497269674cc906c571d9ca1dbfe1e08d23
baeggoose <bsj154@gmail.com>
2024-10-12 20:51:03 +0900

    chore: 클라이언트 baseURL env 수정

--- client/src/utils/apiConfig.ts
+++ client/src/utils/apiConfig.ts
@@ -1,1 +1,3 @@
-export const baseURL = `${process.env.REACT_APP_API_URL_PROD}/api`;
+// export const baseURL = `${process.env.REACT_APP_API_URL_LOCAL}/api`;
+// export const baseURL = `${process.env.REACT_APP_API_URL_DEV}/api`;
+export const baseURL = `${process.env.REACT_APP_API_URL_PROD}api`;
